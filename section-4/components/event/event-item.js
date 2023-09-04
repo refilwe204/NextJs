d43890f2f596214f0639ce784ae40331696727ce
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-05 00:11:57 +0200

    edits to event-item.js

--- section-4/components/event/event-item.js
+++ section-4/components/event/event-item.js
@@ -6,31 +6,31 @@ function EventItem(props){
 
     const humanReadableDate = new Date(date).toLocaleDateString('en-US',{
         day: 'numeric',
-        month: 'numeric',
+        month: 'long',
         year: 'numeric',
-    })
-    const formattedAddress = location.replace(', ', '\n')
+    });
+    const formattedAddress = location.replace(', ', '\n');
     const exploreLink = `/events/${id}`;
 
-    return 
+    return  (
     <li className={classes.item}>
       <img src={'/' + image} alt={title} />
       <div className={classes.content}>
         <div className={classes.summary}>
            <h2>title</h2>
         <div className={classes.date}>
-        <time>humanReadableDate</time>
+        <time>{humanReadableDate}</time>
         </div>
         <div className={classes.address}>
-           <address>formattedAddress</address> 
+           <address>{formattedAddress}</address> 
         </div>
       </div>
       <div classes={classes.actions}>
         <Link href={exploreLink}>Explore Event</Link>
       </div>
       </div>
-        </li>;
-   
+        </li>
+    );
 }
 
 export default EventItem;
