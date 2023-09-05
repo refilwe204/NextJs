f32d89756e334a5709684ae3e11a5c908aca2736
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-05 15:52:19 +0200

    edit to the index.js

--- section-4/pages/events/index.js
+++ section-4/pages/events/index.js
@@ -2,15 +2,24 @@ import { getAllEvents } from "../../dummy-data";
 import EventList from '../../components/event/event-list';
 import EventsSearch from "../../components/event/events-search";
 import { Fragment } from "react";
+import { useRouter } from 'next/router';
 
 function AllEventsPage(){
+  const router=useRouter();
+  const events = getAllEvents();
+ 
 
-    const events = getAllEvents()
+  function findEventsHandler(year, month){
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+   
+  }
 
     return (
-        
+
         <Fragment>
-             <EventsSearch />  
+             <EventsSearch onSearch={findEventsHandler}/>  
             <EventList items={events} />
         </Fragment>
            
