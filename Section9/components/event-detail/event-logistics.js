7a28c65f32d26e0ac94237451b669526325f1afe
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-13 16:22:11 +0200

    edits

---
+++ Section9/components/event-detail/event-logistics.js
@@ -0,0 +1,34 @@
+import AddressIcon from "../icons/address-icon";
+import Image from "next/image";
+import DateIcon from "../icons/date-icon";
+import LogisticsItem from "./logistics-item";
+import classes from "./event-logistics.module.css";
+
+function EventLogistics(props) {
+  const { date, address, image, imageAlt } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const addressText = address.replace(", ", "\n");
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes.image}>
+        <Image src={"/" + image} width={300} height={300} />
+      </div>
+      <ul className={classes.list}>
+        <LogisticsItem icon={DateIcon}>
+          <time>{humanReadableDate}</time>
+        </LogisticsItem>
+        <LogisticsItem icon={AddressIcon}>
+          <address>{addressText}</address>
+        </LogisticsItem>
+      </ul>
+    </section>
+  );
+}
+
+export default EventLogistics;
