1c0ea1be86011793c23d5e88cb4949c58a3123db
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-14 06:54:19 +0200

    added a dynamic eventid component

---
+++ Section9/pages/api/newsletter.js
@@ -0,0 +1,15 @@
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes('@')) {
+        res.status(422).json({message: 'invalid email address'});
+      return; 
+    }
+
+    console.log(userEmail);
+    res.status(201).json({message: 'signed up'})
+  }
+}
+
+export default handler;
