70886db925c6900507fa95a068133041b747fef7
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-15 00:58:35 +0200

    changes to the layout,js

--- section10/pages/_app.js
+++ section10/pages/_app.js
@@ -3,9 +3,11 @@ import Head from "next/head";
 import Layout from "../components/layout/layout";
 import Notification from '../components/ui/notification';
 import "../styles/globals.css";
+import NotificationContextProvider from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <NotificationContextProvider>
     <Layout>
       <Head>
         <title>MeetUp Events</title>
@@ -13,8 +15,8 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
-      <Notification title="Test" message="This is a test." status="error" />
     </Layout>
+    </NotificationContextProvider>
   );
 }
 
