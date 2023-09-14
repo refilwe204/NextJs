846cbf3a140c47cb8c890009a523769f3c99e735
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-14 13:46:26 +0200

    changes

--- Section9/components/input/comments.js
+++ Section9/components/input/comments.js
@@ -8,17 +8,6 @@ function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
-  const [comments, setComments] =useState([]);
-
-  useEffect(() => {
-    if (showComments) {
-      fetch('/api/comments/' + eventId)
-      .then(response => response.json())
-      .then((data) => {
-        setComments(data.comments);
-      });
-    }
-  }, [showComments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
@@ -44,7 +33,7 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items= {comments} />}
+      {showComments && <CommentList />}
     </section>
   );
 }
