3f75c80ecadb437e728343b6392966345c97db45
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-18 14:37:31 +0200

    added an image to my public folder inside site

---
+++ section11/components/home-page/hero.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+import classes from './hero.module.css';
+
+function Hero() {
+    return(
+        <section className={classes.hero}>
+            <div className={classes.image}>Image
+            <Image 
+            src="/images/site/angie.png" 
+            alt="An image showing Refilwe" 
+            width={300} 
+            height={300} 
+            />
+            </div>
+            <h1>Hi, i'm Refilwe</h1>
+            <p> 
+                I blog about Web development - especially frontend frameworks like React or NextJS.
+            </p>
+        </section>
+    )
+}
+
+export default Hero;
