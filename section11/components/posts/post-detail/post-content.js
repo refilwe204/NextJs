d9436b560157c95d0be065a4b20ad5db8538d628
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-20 14:47:46 +0200

    added a react-markdown dependency

--- section11/components/posts/post-detail/post-content.js
+++ section11/components/posts/post-detail/post-content.js
@@ -1,19 +1,22 @@
+import ReactMarkdown from 'react-markdown';
+
 import PostHeader from './post-header';
+import classes from './post-content.module.css';
 
 const DUMMY_POST = {
       slug: 'getting-started-with-nextjs',
       title: 'Getting Started with NextJS',
       image: 'getting-started-nextjs.png',
       date: '2022-02-10',
-      content: '# This is a first post'
+      content: '# This is a first post',
     };
 
 function PostContent() {
     const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
 
-    return <article>
+    return <article className={classes.content}>
         <PostHeader title={DUMMY_POST.title} image={imagePath}/>
-        {DUMMY_POST.content}
+        <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>    
     </article>
 }
 
---
+++ section11/posts/getting-started-with-nextjs.md
@@ -0,0 +1,9 @@
+---
+title: 'Getting-started-with-nextJS'
+date: '2023/09/21'
+image: 'getting-started-nextjs.png'
+---
+
+## This is a title
+
+This is some regular text with a [link](https://google.com)
