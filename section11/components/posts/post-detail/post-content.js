556d80078ccaf56cf6a8d6cc64abbcd612a39b1d
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-20 20:45:10 +0200

    added a markdown

--- section11/components/posts/post-detail/post-content.js
+++ section11/components/posts/post-detail/post-content.js
@@ -11,22 +11,44 @@ function PostContent(props) {
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
     const customRenderers = {
-      img(image) {
-        return (
-        <Image 
-        src={`/images/posts/${post.slug}/${image.src}`}
-        alt={image.alt}
-        width={600} 
-        height={300} 
-        />
-        );
-      },
-    };
+    //   img(image) {
+    //     return (
+    //     <Image 
+    //     src={`/images/posts/${post.slug}/${image.src}`}
+    //     alt={image.alt}
+    //     width={600} 
+    //     height={300} 
+    //     />
+    //     );
+    //   },
+      p(paragraph) {
+        const { node } = paragraph;
 
-    return <article className={classes.content}>
+        if (node.children[0].tagName === 'img') {
+            const image = node.children[0];
+    
+            return (
+              <div className={classes.image}>
+                <Image
+                  src={`/images/posts/${post.slug}/${image.properties.src}`}
+                  alt={image.alt}
+                  width={600}
+                  height={300}
+               />
+            </div>
+        );    
+    }
+
+      return <p>{paragraph.children}</p>; 
+    },
+  };
+
+    return (
+    <article className={classes.content}>
         <PostHeader title={post.title} image={imagePath}/>
         <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>    
     </article>
-}
+    )
+  }
 
-export default PostContent;
+  export default PostContent;
