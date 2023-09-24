17bac5b4d1fc682339e7aa3c7a18aaee0e2cfb32
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-24 21:48:04 +0200

    installed a bcryptjs dependency

--- section13/lib/db.js
+++ section13/lib/db.js
@@ -1,5 +1,9 @@
 import { MongoClient} from 'mongodb'
 
-function connectToDatabase () {
-    MongoClient.connect('');
+export async function connectToDatabase () {
+    const client =  await MongoClient.connect(
+        'mongodb+srv://refilwe101:L4Ysqn4iNX5k1WrH@cluster0.jvoxrhm.mongodb.net/?retryWrites=true&w=majority&auth-demo=AtlasApp'
+        );
+
+    return client;
 }
--- section13/pages/api/auth/signup.js
+++ section13/pages/api/auth/signup.js
@@ -1,3 +1,39 @@
-function handler() {}
+import { hashPassword } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+
+async function handler(req, res) {
+    if (req.method === 'POST') {
+      return;
+    }
+    
+    const data = req.body;
+
+    const { email, password } = data;
+
+    if (!email || 
+        !email.includes('@') || 
+        !password || 
+        password.trim().length <7
+        ) {
+        res
+        .status(422)
+        .json({message: 'Invalid input - password should be at least 7 characters long.',
+    });
+        return;
+    }
+
+    const client = await connectToDatabase();
+
+    const db = client.db();
+
+    const hashedPassword = hashPassword(password);
+
+    const result = await db.collection('users').insertOne({
+        email: email,
+        password: hashedPassword
+    });
+
+    res.status(201).json({ message: 'Created user!'});
+}
 
 export default handler;
