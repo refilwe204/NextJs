4d7c0f94a4a3d0af563016dea1d81938ada9479e
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-24 16:38:33 +0200

    working with environment variables

---
+++ section12/pages/api/contact.js
@@ -0,0 +1,57 @@
+import {MongoClient} from 'mongodb'
+
+async function Handler(req, res) {
+  if ( req.method === 'POST') {
+  const {email, name, message } = req.body;
+
+  if(
+    !email ||
+    !email.includes('@') ||
+    !name ||
+    name.trim === '' ||
+    !message ||
+    message.trim() === ''
+  ) {
+    res.status(422).json({ message: 'Invalid input'});
+    return;
+  }
+
+  const newMessage = {
+    email,
+    name,
+    message,
+  };
+
+  let client;
+
+  try {
+  const client = await MongoClient.connect(
+    'mongodb+srv://refilwe101:TcSwxxeG4jG8GaKV@cluster0.jvoxrhm.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
+  );
+  
+
+ } catch (error) {
+   res.status(500).json({message: 'Could not connect to database'});
+   return;
+ }
+
+ const db = client.db();
+
+ try {
+ const result = await db.collection('messages').insertOne(newMessage);
+  newMessage.id = result.insertedId;
+} catch (error) {
+  client.close();
+   res.status(500).json({message: 'Storing message failed'});
+   return;
+}
+
+client.close();
+
+  res.status(201)
+  .json({ message: 'Successfully stored message', message: newMessage});
+} 
+}
+
+export default Handler;
+
