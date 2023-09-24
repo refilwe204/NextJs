6427428b0def33d29a9ce61193dfd4bd3dd6c36a
refilwe204 <nqatyelwaangelin@gmail.com>
2023-09-24 15:34:23 +0200

    added a lang attribute to my page source

--- section11/components/contact/contact-form.js
+++ section11/components/contact/contact-form.js
@@ -1,92 +1,92 @@
 import { useEffect, useState } from 'react';
-
 import classes from './contact-form.module.css';
 import Notification from '../ui/notification';
 
 async function sendContactData(contactDetails) {
-    const response = await fetch('/api/contact', {
-        method: 'POST',
-        body: JSON.stringify(contactDetails),
-        headers: {
-         'Content-Type': 'application/json',
-        },
-     });
+  const response = await fetch('/api/contact', {
+    method: 'POST',
+    body: JSON.stringify(contactDetails),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-     const data = await response.json();
+  const data = await response.json();
 
-     if (!response.ok) {
-         throw new Error(data.message || 'Something went wrong!');
-     }
- }
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong!');
+  }
+}
 
 function ContactForm() {
-    const [enteredEmail, setEnteredEmail] = useState('');
-    const [enteredName, setEnteredName] = useState('');
-    const [enteredMessage, setEnteredMessage] = useState('');
-    const [requestStatus, setRequestStatus] = useState();
-    const [requestError, setRequestError] =  useState();
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredMessage, setEnteredMessage] = useState('');
+  const [requestStatus, setRequestStatus] = useState();
+  const [requestError, setRequestError] = useState();
 
-    useEffect(() =>{
-        if (requestStatus === 'success' || requestStatus === 'error') {
-            const timer = setTimeout(() =>{
-                setRequestStatus(null);
-                setRequestError(null);
-            }, 3000);
+  useEffect(() => {
+    if (requestStatus === 'success' || requestStatus === 'error') {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
 
-            return () => clearTimeout(timer);
-        }
-    }, [requestStatus]);
-
-    async function sendMessageHandler(event) {
-        event.preventDefault();
-
-        setRequestStatus('pending');
-
-        try{
-        await sendContactData({
-            email: enteredEmail,
-            name: enteredName, 
-            message: enteredMessage,
-        });
-        setRequestStatus('success');
-        setEnteredMessage('');
-        setEnteredEmail('');
-        setEnteredName('');
-      } catch (error) {
-        setRequestError(error.message);
-        setRequestStatus('error');
-      }
-
-      let notification;
-
-      if (requestStatus === 'pending') {
-        notification = {
-            status: 'pending',
-            title: 'Sending message...',
-            message: 'Your message is on its way!'
-        };
-      }
-
-      if (requestStatus === 'success') {
-        notification = {
-            status: 'success',
-            title: 'Success!',
-            message: 'Message sent successfully!'
-      };
+      return () => clearTimeout(timer);
     }
+  }, [requestStatus]);
 
-    if (requestStatus === 'error') {
-        notification = {
-            status: 'error',
-            title: 'Error!',
-            message: requestError,
-      };
+  async function sendMessageHandler(event) {
+    event.preventDefault();
+
+    setRequestStatus('pending');
+
+    try {
+      await sendContactData({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      });
+      setRequestStatus('success');
+      setEnteredMessage('');
+      setEnteredEmail('');
+      setEnteredName('');
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus('error');
     }
+  }
 
-    return (
-        <section className={classes.contact}>
-            <h1>How can I help you?</h1>
-            <form className={classes.form} onSubmit={sendMessageHandler}>
+  let notification;
+
+  if (requestStatus === 'pending') {
+    notification = {
+      status: 'pending',
+      title: 'Sending message...',
+      message: 'Your message is on its way!',
+    };
+  }
+
+  if (requestStatus === 'success') {
+    notification = {
+      status: 'success',
+      title: 'Success!',
+      message: 'Message sent successfully!',
+    };
+  }
+
+  if (requestStatus === 'error') {
+    notification = {
+      status: 'error',
+      title: 'Error!',
+      message: requestError,
+    };
+  }
+
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
                 <div className={classes.controls}>
                     <div className={classes.control}> 
                     <label htmlFor='email'>Your Email</label>
@@ -124,17 +124,15 @@ function ContactForm() {
                 <button>Send Message</button>
                 </div>
             </form>
-            {notification && (
-            <Notification 
-            status={notification.status} 
-            title={notification.title} 
-            message={notification.message} 
-            />
-           )}
-        </section>
-    );
-
-  };
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+    </section>
+  );
 }
 
 export default ContactForm;
--- section11/pages/posts/[slug].js
+++ section11/pages/posts/[slug].js
@@ -1,8 +1,21 @@
+import Head from 'next/head';
+import { Fragment } from 'react';
+
 import PostContent from '../../components/posts/post-detail/post-content';
 import { getPostData, getPostsFiles } from '../../lib/post-util';
 
 function PostDetailPage(props) {
-    return <PostContent post={props.post} />;
+    return (
+        <Fragment>
+            <Head>
+                <title>{props.post.tittle}</title>
+                <meta name="description" 
+                content= {props.post.excerpt} 
+               />
+            </Head>
+            <PostContent post={props.post} />
+        </Fragment>
+    ) 
 }
 
 export function getStaticProps(context) {
